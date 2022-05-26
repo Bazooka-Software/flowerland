6259b847de5ba2ff7cf6362f5f4301a6228eb94d
Valdas Vabolis <thisisvaldasvabolis@gmai.com>
2022-05-26 23:30:37 +0300

    feat: spring security

---
+++ src/main/java/com/bazooka/flowerland/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.bazooka.flowerland;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            .antMatchers("/**", "/index**").permitAll() // prideti URL'us kuriuos gali matyt visi
+            .antMatchers("/login**").permitAll() // prideti URL'us kuriuos gali matyti tik admin
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+            .and()
+            .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
+                .roles("ADMIN");
+    }
+
+}
