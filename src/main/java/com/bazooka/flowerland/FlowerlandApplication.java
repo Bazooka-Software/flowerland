574762d31e0f2fb78b0ec284e257b515a487c8eb
Valdas Vabolis <thisisvaldasvabolis@gmai.com>
2022-05-27 23:04:10 +0300

    Add interceptor

--- src/main/java/com/bazooka/flowerland/FlowerlandApplication.java
+++ src/main/java/com/bazooka/flowerland/FlowerlandApplication.java
@@ -1,9 +1,11 @@
 package com.bazooka.flowerland;
 
+import com.bazooka.flowerland.interceptors.LoggerInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @SpringBootApplication
 public class FlowerlandApplication extends SpringBootServletInitializer {
